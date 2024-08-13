6cd070a9ad0d6207c69dc077c654b7d2405e7ded
Shahab-16 <mdshahabuddin0516@gmail.com>
2024-08-14 01:22:36 +0530

    Add React frontend with Tailwind CSS

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-blue-900 text-fuchsia-50 text-[4rem] h-[100vh] w-full flex justify-center items-center font-bold'>
+      <h1>MEDISENSE WELCOMES YOU</h1>
+    </div>
+  );
+}
+
+export default App;
