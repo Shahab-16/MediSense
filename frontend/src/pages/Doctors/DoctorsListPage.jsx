cdf0a6551f591c37b13177c307bbd5bc20a8058b
Shahab-16 <mdshahabuddin0516@gmail.com>
2024-12-16 01:27:00 +0530

    updated doctorlistpage

--- frontend/src/pages/Doctors/DoctorsListPage.jsx
+++ frontend/src/pages/Doctors/DoctorsListPage.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import AllDoctorsList from '../../components/Doctors/AllDoctorsList'
+import React from 'react';
+import AllDoctorsList from '../../components/Doctors/AllDoctorsList';
+import { doctor_specializations } from '../../assets/asset';
+
 const DoctorsListPage = () => {
   return (
-    <div className='flex'>
-      <div className='mt-[3%]'>
-      <p className='p-2'>Browse through the doctors specialist.</p>
-        <p className='border p-2 rounded-md font-outfit m-2 cursor-pointer'>General physician</p>
-        <p className='border p-2 rounded-md font-outfit m-2 cursor-pointer'>Dermatologist</p>
-        <p className='border p-2 rounded-md font-outfit m-2 cursor-pointer'>Pediatricians</p>
-        <p className='border p-2 rounded-md font-outfit m-2 cursor-pointer'>Neurologist</p>
-        <p className='border p-2 rounded-md font-outfit m-2 cursor-pointer'>Gastroenterologist</p>
+    <div className="flex">
+      {/* Specializations Sidebar */}
+      <div className="mt-[3%] w-1/4">
+        <p className="p-2 text-black font-bold">Browse through the doctors specialist.</p>
+        {doctor_specializations.map((specialization, index) => (
+          <p
+            key={index}
+            className="border p-2 rounded-md font-outfit m-2 cursor-pointer hover:bg-blue-50 hover:text-blue-600"
+          >
+            {specialization.name}
+          </p>
+        ))}
       </div>
-      <div className='mt-[3%]'>
-        <AllDoctorsList/>
+      
+      {/* Doctors List */}
+      <div className="mt-[3%] w-3/4">
+        <AllDoctorsList />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DoctorsListPage
+export default DoctorsListPage;
