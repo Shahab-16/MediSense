1c43df6d214adbc0e388b90a5b97c21209cac4ec
Shahab-16 <mdshahabuddin0516@gmail.com>
2024-12-20 14:57:42 +0530

    add admin template

---
+++ admin/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-5xl font-semibold">WELCOME TO THE ADMIN SECTION OF <span className="text-blue-600 font-extrabold">MEDISENSE</span></h1>
+    </div>
+  );
+}
+
+export default App;
